a7eb575c83c70d7547e78c93751ef04577dd4591
Luis <lwolf95@hotmail.com>
2018-04-07 14:51:07 -0500

    Validaciones terminadas

---
+++ app/src/main/java/com/example/luisanibal/calculadora/Metodos.java
@@ -0,0 +1,16 @@
+package com.example.luisanibal.calculadora;
+
+public class Metodos {
+    public static double Sumar(double n1, double n2){
+        return n1+n2;
+    }
+    public static double Restar(double n1, double n2){
+        return n1-n2;
+    }
+    public static double Multiplicar(double n1, double n2){
+        return n1*n2;
+    }
+    public static double Dividir(double n1, double n2){
+        return n1/n2;
+    }
+}
